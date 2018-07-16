8892e5bd146abda2fa517fefaa6f13167f190128
freewind <nowindlee@gmail.com>
2018-07-16 21:57:51 +0800

    JsObj

--- src/main/kotlin/helpers.kt
+++ src/main/kotlin/helpers.kt
@@ -1,3 +1,9 @@
-@file:Suppress("unused", "NOTHING_TO_INLINE", "FunctionName")
+@file:Suppress("unused", "NOTHING_TO_INLINE", "FunctionName", "UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
 
 inline fun <T> JsObj(): T = js("{}") as T
+inline fun JsObj(): JsObj = js("{}") as JsObj
+
+external interface JsObj {
+    operator fun get(key: String): dynamic
+    operator fun set(s: String, value: dynamic)
+}
